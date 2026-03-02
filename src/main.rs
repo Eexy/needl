7580c5f166538c581c680cfa1310f2f6149c7372
eexy <eexdhe@gmail.com>
2026-03-02 18:18:15 +0100

    feat: add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{
+    env::{self},
+    process,
+};
+
+#[derive(Debug)]
+struct Config {
+    file: String,
+    pattern: String,
+}
+
+impl Config {
+    fn new(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+        args.next();
+        let file = match args.next() {
+            Some(file) => file,
+            None => return Err("no argument passed for file"),
+        };
+
+        let pattern = match args.next() {
+            Some(pattern) => pattern,
+            None => return Err("no argument passed for pattern"),
+        };
+
+        Ok(Config { file, pattern })
+    }
+}
+
 fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    dbg!(config);
     println!("Hello, world!");
 }
